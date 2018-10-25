130b508fc5a23b24c080ea18da62d4578c556646
AWU20181023 <18578275545@163.com>
2018-10-25 21:00:42 +0800

    初步提交

--- src/main/java/com/gree/production/controller/ProductionController.java
+++ src/main/java/com/gree/production/controller/ProductionController.java
@@ -1,5 +1,7 @@
 package com.gree.production.controller;
 
+import com.gree.production.entity.dto.Result;
+import com.gree.production.entity.po.Category;
 import com.gree.production.service.ProductionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +12,7 @@ public class ProductionController {
     @Autowired
     private ProductionService productionService;
 
+    public Result getList() {
+        return null;
+    }
 }
